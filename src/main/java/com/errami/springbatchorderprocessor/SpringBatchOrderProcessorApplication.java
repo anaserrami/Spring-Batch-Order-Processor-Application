495e4ffd5e175cadd904260c63011a9011c55575
anaserrami <ramianas314@gmail.com>
2024-12-20 22:26:33 +0100

    add main class

--- src/main/java/com/errami/springbatchorderprocessor/SpringBatchOrderProcessorApplication.java
+++ src/main/java/com/errami/springbatchorderprocessor/SpringBatchOrderProcessorApplication.java
@@ -2,12 +2,16 @@ package com.errami.springbatchorderprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBatchOrderProcessorApplication {
 
     public static void main(String[] args) {
+        System.out.println("FILE : " + new ClassPathResource("orders.csv").exists());
+
         SpringApplication.run(SpringBatchOrderProcessorApplication.class, args);
     }
-
 }
